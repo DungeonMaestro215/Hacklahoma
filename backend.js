6ee4245dc4c47388a4ab01d8f1ae4ca4854d281d
Denver Massey <denmas22@live.unc.edu>
2021-02-06 20:33:58 -0500

    added some console logs

--- backend.js
+++ backend.js
@@ -19,11 +19,14 @@ app.use(cors());
 app.get('/static/authenticate', async function(req,res) {
     //run the api to auth spotify
     let test = await getAuth();
+    console.log('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + test.client_id + '&redirect_uri=' + encodeURIComponent(test.redirect_url));
     res.redirect('https://accounts.spotify.com/authorize' +
     '?response_type=code' +
     '&client_id=' + test.client_id + '&redirect_uri=' + encodeURIComponent(test.redirect_url));
     // res.send(Object.keys(test.));
-    res.end();
+    // res.end();
     // console.log("RIGHT HERE HOMIE --------------------------------------------------------------------------");
     // console.log(Object.keys(test.request._redirectable._currentUrl));
     // res.send(test);
