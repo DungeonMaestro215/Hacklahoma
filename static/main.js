b9452faa64618b3ad3ac34ac9ca2938946d636ff
Logan Qualkenbush <loganq@gmail.com>
2021-02-06 21:26:37 -0500

    did this do anything

--- static/main.js
+++ static/main.js
@@ -6,9 +6,26 @@ window.onload = () => {
 
     } else {
     
-        document.getElementById("linkSpotifyButton").addEventListener("click", function() {
-            linkSpotify();
-        });
+    document.getElementById("linkSpotifyButton").addEventListener("click", function() {
+        linkSpotify();
+    });
+
+}
+
+function randomCode(length) {
+    var result= '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+   return result;
+}
+
+function inputCode(code) {
+    var code = code;
+    if (code.length != 5) {
+        return error;
     }
 }
 
@@ -39,3 +56,11 @@ function linkSpotify() {
     // window.location = window.location + "static/authenticate";
     window.location = "https://hacklahoma2.herokuapp.com/static/authenticate";
 }
+    // document.getElementById("groupButtons").innerHTML = newGroupButtons;
+    // document.getElementById("createGroupButton").addEventListener("click", function() {alert("Your code is: " + randomCode(5));})
+    // document.getElementById("joinGroupButton").addEventListener('click', function() {
+    //     inputCode(code);
+    // })
+    // document.getElementById('test').addEventListener('click', communicator);
+}
+
