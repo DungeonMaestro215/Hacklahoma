d67cdea270daf2e92d9918fce36922eee3e669cd
Simon Post <postse@live.unc.edu>
2021-02-07 04:27:33 -0500

    tryin again again again

--- static/apicalls.js
+++ static/apicalls.js
@@ -17,8 +17,10 @@ async function getUserInfo(accessCode) {
 }
 
 async function getToken(code) {
+    console.log("Code" + code)
     try  {
-        console.log(atob(CLIENT_ID  + ":" + CLIENT_SECRET));
+        console.log("test")
+        // console.log(atob(CLIENT_ID  + ":" + CLIENT_SECRET));
         console.log(btoa(CLIENT_ID  + ":" + CLIENT_SECRET));
         const results = await axios({
             method: 'post',
